6a3f5babf2352b6767a5c61e3597d36c045c05b3
MyEmulator56 <34338246+MyEmulator56@users.noreply.github.com>
2017-12-07 10:35:55 +0000

    Create main_class.java

---
+++ main_class.java
@@ -0,0 +1,8 @@
+
+public class main_class {
+
+  public static void main(String[] args) {
+    System.out.println("Hello , World!"); //infamous hello world. just to fill up my new repository  
+  
+  }
+ }
